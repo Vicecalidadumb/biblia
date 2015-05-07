633406ba8b0982d4ea2730c814f869d667cef12d
Yeison Arias <yeison.arias@umb.edu.co>
2015-05-07 08:33:09 -0500

    Cambios al controlador principal
    Cambios al controlador principal

--- application/controllers/principal.php
+++ application/controllers/principal.php
@@ -5,18 +5,59 @@ if (!defined('BASEPATH'))
 
 class Principal extends CI_Controller {
 
-    private $module_sigla;
-
     public function __construct() {
         parent::__construct();
         $this->load->helper('miscellaneous');
         $this->load->model('principal_model');
     }
 
-    public function asignacion() {
-        //$data['registros'] = $this->user_model->get_all_users('ALL');
-        //$this->load->view('template/template', $data);
+    public function index() {
         echo "el viejo sergio";
     }
 
+    public function asignacion() {
+        $SQL= 'INSERT INTO umbpruebas_inscritos_asignados values ';
+        
+        //1 paso: consultamos las ciudades
+        $ciudades = $this->principal_model->traer_ciudades();
+        //2 paso: creamos ciclo con las ciudades:
+        foreach ($ciudades as $ciudad){
+            //3 paso: traemos los inscritos por la ciudad
+            $inscritos_ciudad = $this->principal_model->traer_inscritos_ciudad($ciudad->ciudad_id);
+            
+            //4 paso: traemos los espacios por la ciudad
+            $espacio_ciudad = $this->principal_model->traer_espacios_ciudad($ciudad->ciudad_id);
+            //5 paso: generamos el array con los espacios
+            $espacios = crear_array_esp($espacio_ciudad);
+            //6 paso: generamos el SQL para asignar los inscritos
+            
+            $contador = 0;
+            foreach ($inscritos_ciudad as $inscrito){
+                $SQL.='(';
+                $SQL.=$inscrito->inscrito_id;
+                $SQL.=",'";
+                $SQL.=$inscrito->inscrito_pin;
+                $SQL.="','";
+                $SQL.=$inscrito->inscrito_cedula;
+                $SQL.="','";
+                $SQL.=$inscrito->inscrito_nombre;
+                $SQL.="','";
+                $SQL.=$espacios[$contador]['institucion_nombre'];
+                $SQL.="','";
+                $SQL.=$espacios[$contador]['puesto'];
+                $SQL.="','";
+                $SQL.=$espacios[$contador]['ciudad'];
+                $SQL.="','";
+                $SQL.=$espacios[$contador]['espacio_bloque'];
+                $SQL.="','";
+                $SQL.=$espacios[$contador]['espacio_piso'];
+                $SQL.="','";
+                $SQL.=$espacios[$contador]['espacio_aula'];
+                $SQL.="'),";
+                $contador++;
+            }
+        }
+        echo $SQL;
+        $this->principal_model->insertar(trim($SQL,',').';');
+    }
 }
--- application/models/principal_model.php
+++ application/models/principal_model.php
@@ -4,26 +4,56 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Principal_model extends CI_Model {
-    
-    
-    public function get_user($username){
+    /*
+     * public function traer_ciudades(){
+      $this->db->select('*');
+      $this->db->where('USUARIO_NUMERODOCUMENTO',$username);
+      $this->db->where('USUARIO_ESTADO','1');
+      $query = $this->db->get('usuarios');
+      $datos=$query->result();
+      if(count($datos)==0){
+      $moodle_db = $this->load->database('moodle', TRUE);
+      $moodle_db->select("id USUARIO_ID,username USUARIO_PASSWORD, firstname USUARIO_NOMBRES,lastname USUARIO_APELLIDOS,'' USUARIO_TIPODOCUMENTO, username USUARIO_NUMERODOCUMENTO,
+      email USUARIO_CORREO, '' USUARIO_GENERO, '' USUARIO_FECHADENACIMIENTO,'' USUARIO_DIRECCIONRESIDENCIA, '' USUARIO_TELEFONOFIJO, '' USUARIO_CELULAR,
+      '' USUARIO_ESTADO, '' USUARIO_FECHAINGRESO, 'estudiante' ID_TIPO_USU");
+      $moodle_db->where('username',$username);
+      $query =$moodle_db->get('pruebas_user');
+      $datos=$query->result();
+      }
+      //        echo print_y($query->result());die;
+      return $datos;
+      }
+     */
+
+    public function traer_ciudades() {
         $this->db->select('*');
-        $this->db->where('USUARIO_NUMERODOCUMENTO',$username);
-        $this->db->where('USUARIO_ESTADO','1');
-        $query = $this->db->get('usuarios');
-        $datos=$query->result();
-        if(count($datos)==0){
-            $moodle_db = $this->load->database('moodle', TRUE);
-            $moodle_db->select("id USUARIO_ID,username USUARIO_PASSWORD, firstname USUARIO_NOMBRES,lastname USUARIO_APELLIDOS,'' USUARIO_TIPODOCUMENTO, username USUARIO_NUMERODOCUMENTO,
-                email USUARIO_CORREO, '' USUARIO_GENERO, '' USUARIO_FECHADENACIMIENTO,'' USUARIO_DIRECCIONRESIDENCIA, '' USUARIO_TELEFONOFIJO, '' USUARIO_CELULAR,
-                '' USUARIO_ESTADO, '' USUARIO_FECHAINGRESO, 'estudiante' ID_TIPO_USU");
-            $moodle_db->where('username',$username);
-            $query =$moodle_db->get('pruebas_user');
-            $datos=$query->result();
-        }
-//        echo print_y($query->result());die;
+        $this->db->where('ciudad_id', '4');
+        $query = $this->db->get('umbpruebas_ciudades');
+        $datos = $query->result();
         return $datos;
     }
-    
-    
+
+    public function traer_inscritos_ciudad($ciudad_id) {
+        $this->db->select('*');
+        $this->db->where('ciudad_id_presentacion', $ciudad_id);
+        $query = $this->db->get('umbpruebas_inscritos');
+        $datos = $query->result();
+        return $datos;
+    }
+
+    public function traer_espacios_ciudad($ciudad_id) {
+        $this->db->select('*');
+        $this->db->where('umbpruebas_instituciones.ciudad_id', $ciudad_id);
+        $this->db->join('umbpruebas_instituciones', 'umbpruebas_instituciones.institucion_id = umbpruebas_espacios.institucion_id', 'left');
+        $this->db->join('umbpruebas_ciudades', 'umbpruebas_ciudades.ciudad_id = umbpruebas_instituciones.ciudad_id', 'left');
+        $query = $this->db->get('umbpruebas_espacios');
+        $datos = $query->result();
+        return $datos;
+    }
+
+    public function insertar($sql) {
+        $SQL_string_query = $this->db->query($sql);
+        //$SQL_string_query->result();
+    }
+
 }
